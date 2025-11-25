578282893d07438730ed045abbdeda2eb84ec746
Muhammet Sadik Arici <muarici@k1m12s03.42kocaeli.com.tr>
2025-11-26 00:52:59 +0300

    parse mid

--- map_parser.c
+++ map_parser.c
@@ -6,11 +6,36 @@
 /*   By: muarici <muarici@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 00:46:31 by muarici           #+#    #+#             */
-/*   Updated: 2025/11/26 00:46:41 by muarici          ###   ########.fr       */
+/*   Updated: 2025/11/26 00:52:44 by muarici          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+char *skip_first_line(char *content)
+{
+	int i;
+
+	i = 0;
+	while (content[i] && content[i] != '\n')
+	{
+		i++;
+	}
+	if (content[i] == '\0')
+	{
+		return (NULL);
+	}
+	return (content + i + 1);
+}
+
 int get_line_length(char *map_start)
 {
-	
+    int i;
+
+    i = 0;
+    while (map_start[i] && map_start[i] != '\n')
+    {
+        i++;
+    }
+    return (i);
 }
